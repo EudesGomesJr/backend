8096c0bc0fc7e6f2454c133aad64f000f90bb3f7
Eudes Gomes <eudes_jr@hotmail.com>
2024-10-26 11:55:05 -0300

    atualizações em 26/10/2024

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -4,42 +4,52 @@ const CreateUser = async (request, response) => {
   try {
     await UserModel.create(request.body);
     response.status(201);
-    response.json({ message: "Usuário criado com sucesso" }); // Como o padão de comunicação numa ApiRest é Json
+    return response.json({ message: "Usuário criado com sucesso" }); // Como o padão de comunicação numa ApiRest é Json
   } catch (error) {
     console.log(error.message);
     response.status(400); // Erro dos dados informados pelo usuário
-    response.json({ message: "Erro ao criar usuário" });
+    return response.json({ message: "Erro ao criar usuário" });
   }
 };
 
 const ListUsers = async (request, response) => {
   try {
-    const users = await UserModel.findAll();
+    const users = await UserModel.findAll({
+        attributes: {
+            exclude:["password"]
+        }}
+    );
     response.status(200);
-    response.json(users);
+    return response.json(users);
   } catch (error) {
     console.log(error.message);
     response.status(500); // Erro interno, que não depende do usuário
-    response.json({ message: "Erro de conexão com o banco" });
+    return response.json({
+        message: "Ocorreu um erro no servidor. Entre em contato com o suporte.",
+      });
   }
 };
 
 const UserById = async (request, response) => {
   try {
-    const user = await UserModel.findByPk(request.params.id);
+    const user = await UserModel.findByPk(request.params.id,{
+        attributes: {
+            exclude:["password"]
+        }}
+    );
     if (user === null) {
-      response.status(400);
-      response.json({ message: "Usuário não encontrado" });
+      response.status(404);
+      return response.json({ message: "Usuário não encontrado" });
     } else {
-      response.status(200);
-      response.json(user);
+      // Não colocando response.status ele retorna, por padrão, 200
+      // response.status(200);
+      return response.json(user);
     }
-    response.status(200);
   } catch (error) {
     console.log(error.message);
     response.status(500);
-    response.json({
-      message: "Erro de conexão com o banco. Entre em contato com o suporte.",
+    return response.json({
+      message: "Ocorreu um erro no servidor. Entre em contato com o suporte.",
     });
   }
 };
@@ -48,41 +58,90 @@ const UpdateUser = async (request, response) => {
   try {
     const user = await UserModel.findByPk(request.params.id);
     if (user === null) {
-      response.status(400);
-      response.json({ message: "Usuário não encontrado" });
+      response.status(404);
+      return response.json({ message: "Usuário não encontrado" });
     } else {
       user.set(request.body);
       await user.save();
-      response.status(200);
-      response.json({ message: "Usuário atualizado com sucesso" });
+      // Não colocando response.status ele retorna, por padrão, 200
+      // response.status(200);
+      return response.json({ message: "Usuário atualizado com sucesso" });
     }
   } catch (error) {
     console.log(error.message);
     response.status(500);
-    response.json({
+    return response.json({
       message: "Erro ao tentar atualizar dados do usuário",
     });
   }
 };
+
+const UpdateUserKelvys = async (request, response) => {
+    try {
+        let {id} = request.params; // Desestruturação do objeto
+        let {body} = request;
+        let {total} = await UserModel.update(body, {
+            where: {id}
+        });
+        if (total <= 0) {
+            response.status(204);
+            return response.json(
+                { message: "Usuário atualizado com sucesso" }
+            )
+        };
+    } catch (error) {
+        response.status(500);
+        return response.json({
+          message: "Erro ao tentar atualizar dados do usuário",
+        });
+    }
+  };
+
 const DeleteUser = async (request, response) => {
   try {
     const user = await UserModel.findByPk(request.params.id);
     if (user === null) {
       response.status(400);
-      response.json({ message: "Usuário não encontrado" });
+      return response.json({ message: "Usuário não encontrado" });
     } else {
       await user.destroy();
-      response.status(200);
-      response.json({ message: "Usuário deletadp com sucesso" });
+      // Não colocando response.status ele retorna, por padrão, 200
+      // response.status(200);
+      return response.json({ message: "Usuário deletadp com sucesso" });
     }
   } catch (error) {
     console.log(error.message);
     response.status(500);
-    response.json({
+    return response.json({
       message: "Erro ao tentar deletar o usuário",
     });
   }
 };
+
+const DeleteUserKelvys = async (request, response) => {
+    try {
+        let {id} = request.params; // Desestruturação do objeto
+        // No await UserModel.destroy abaixo, o retorno é um número, 
+        // e não um array, como ocorre no update
+        let total = await UserModel.destroy({
+            where: {id}
+        })
+        if (total <= 0) {
+            // Não colocando response.status ele retorna, por padrão, 200
+            // response.status(200);
+            return response.json(
+                { message: "Usuário deletado com sucesso" }
+            )
+        };
+
+    } catch (error) {
+      console.log(error.message);
+      response.status(500);
+      return response.json({
+        message: "Erro ao tentar deletar o usuário",
+      });
+    }
+  };
 
 module.exports = {
   ListUsers,

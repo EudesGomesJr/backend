8a5d5e8b3b0be3b71b6094cfc9f3fb1fc7b80962
EudesGomesJr <eudes_jr@hotmail.com>
2024-11-15 15:30:54 -0300

    Implementado create post by url

--- playground.js
+++ playground.js
@@ -1,16 +1,26 @@
-const fs = require('fs');
-const img = require("./img");
-let url = 'https://picsum.photos/200/300';
+const fs = require("fs");
+const img = `/9j/4QDeRXhpZgAASUkqAAgAAAAGABIBAwABAAAAAQAAABoBBQABAAAAVgAAABsBBQABAAAAXgAAACgBAwABAAAAAgAAABMCAwABAAAAAQAAAGmHBAABAAAAZgAAAAAAAABIAAAAAQAAAEgAAAABAAAABwAAkAcABAAAADAyMTABkQcABAAAAAECAwCGkgcAFgAAAMAAAAAAoAcABAAAADAxMDABoAMAAQAAAP//AAACoAQAAQAAAAoAAAADoAQAAQAAAAoAAAAAAAAAQVNDSUkAAABQaWNzdW0gSUQ6IDM0MP/bAEMACAYGBwYFCAcHBwkJCAoMFA0MCwsMGRITDxQdGh8eHRocHCAkLicgIiwjHBwoNyksMDE0NDQfJzk9ODI8LjM0Mv/bAEMBCQkJDAsMGA0NGDIhHCEyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMv/CABEIAAoACgMBIgACEQEDEQH/xAAXAAADAQAAAAAAAAAAAAAAAAAAAQQF/8QAFQEBAQAAAAAAAAAAAAAAAAAAAQL/2gAMAwEAAhADEAAAAcWUcn//xAAZEAEAAgMAAAAAAAAAAAAAAAACAQMQERL/2gAIAQEAAQUCglHm3dOP/8QAFBEBAAAAAAAAAAAAAAAAAAAAAP/aAAgBAwEBPwF//8QAFBEBAAAAAAAAAAAAAAAAAAAAAP/aAAgBAgEBPwF//8QAGhAAAgIDAAAAAAAAAAAAAAAAAAIBIRAxcf/aAAgBAQAGPwKqY1Ivcf/EABoQAQADAAMAAAAAAAAAAAAAAAEAETEhofD/2gAIAQEAAT8hPt9A9sUXXFzFW9n/2gAMAwEAAgADAAAAELP/xAAVEQEBAAAAAAAAAAAAAAAAAAAAAf/aAAgBAwEBPxCv/8QAFxEBAAMAAAAAAAAAAAAAAAAAAQARIf/aAAgBAgEBPxC0yf/EABwQAQEAAgIDAAAAAAAAAAAAAAERACExUXGBsf/aAAgBAQABPxAxWiXZr416xwBFUKh4xI1hIXi84NApe8//2Q==`;
+let url = "https://picsum.photos/200/300";
 const path = require("path");
 
+const mimeTypeMap = {
+  "image/png": "png",
+  "image/jpg": "jpg",
+  "image/jpeg": "jpeg",
+  "image/webp": "webp",
+  "image/jfif": "jfif",
+};
+
 async function execute() {
-    let response = await fetch(url)
-    let mimeType = response.headers.get('content-type')
-    let buffer = await response.arrayBuffer();
-    buffer = Buffer.from(buffer,'binary');
-    fs.writeFileSync('teste.jpg',buffer, {
-        encoding: 'binary'
-    });
+  let response = await fetch(url);
+  let mimeType = response.headers.get("content-type");
+  let extension = mimeTypeMap[mimeType];
+  let buffer = await response.arrayBuffer();
+  buffer = Buffer.from(buffer, "binary");
+  console.log(buffer);
+  fs.writeFileSync(`teste.${extension}`, buffer, {
+    encoding: "binary",
+  });
 }
 
 execute();
--- src/controllers/PostsController.js
+++ src/controllers/PostsController.js
@@ -1,57 +1,78 @@
 const { resolveObjectURL } = require("buffer");
 const PostsModel = require("../models/PostsModel");
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
 const mimeTypeMap = {
-    "image/png": "png",
-    "image/jpg": "jpg",
-    "image/jpeg": "jpeg",
-    "image/webp": "webp",
-    "image/jfif": "jfif"
-}
+  "image/png": "png",
+  "image/jpg": "jpg",
+  "image/jpeg": "jpeg",
+  "image/webp": "webp",
+  "image/jfif": "jfif",
+};
 const CreatePost = async (request, response) => {
-  let {type, mime, content} = request.body.image;
-  let {user_id, title} = request.body;
+  let { type, mime, content } = request.body.image;
+  let { user_id, title } = request.body;
+
+  let directory = path.resolve("static/images"); // path.resolve é uma forma do node passar o caminho do projeto
+  let filename = Math.random().toString(16).slice(2); //Gera números aleatórios usando hexadecimais
 
   try {
-    let directory = path.resolve('static/images'); // path.resolve é uma forma do node passar o caminho do projeto
-    let filename = Math.random().toString(16).slice(2); //Gera números aleatórios usando hexadecimais 
-
     if (!fs.existsSync(directory)) {
-      fs.mkdirSync(directory, {recursive: true});
+      fs.mkdirSync(directory, { recursive: true });
     }
 
-    if(!content || !type) {
-        throw new Error("Body inválido");
+    if (!content || !type) {
+      throw new Error("Body inválido");
     }
-    if(type === 'base64') {
-        if (!mime) {
-            throw new Error("Mime é obrigatório para o type base64")
-        }
+    if (type === "base64") {
+      if (!mime) {
+        throw new Error("Mime é obrigatório para o type base64");
+      }
 
-        let extension = mimeTypeMap[mime];
-        if(!extension) {
-            throw new Error("Mime inválido")
-        }
+      let extension = mimeTypeMap[mime];
+      if (!extension) {
+        throw new Error("Mime inválido");
+      }
 
-        filename = `${filename}.${extension}`;
-        fs.writeFileSync(`${directory}/${filename}`, atob(content), {
-            encoding: 'binary',
-        });
+      filename = `${filename}.${extension}`;
+      fs.writeFileSync(`${directory}/${filename}`, atob(content), {
+        encoding: "binary",
+      });
+    } else if (type === "url") {
+      let response = await fetch(content);
+      let mimeType = response.headers.get("content-type");
+      let extension = mimeTypeMap[mimeType];
+
+      let buffer = await response.arrayBuffer();
+      buffer = Buffer.from(buffer, "binary");
+
+      filename = `${filename}.${extension}`;
+      console.log(filename);
+      console.log("======");
+      fs.writeFileSync(`${directory}/${filename}`, buffer, {
+        encoding: "binary",
+      });
+    } else {
+      throw new Error("Tipo inválido");
     }
 
-    //return response.json({ message: "Arquivo criado com sucesso" }); 
+    //return response.json({ message: "Arquivo criado com sucesso" });
 
     await PostsModel.create({
       user_id,
       title,
-      image: filename
+      image: filename,
     });
 
     response.status(201);
     return response.json({ message: "Post criado com sucesso" }); // Como o padão de comunicação numa ApiRest é Json
   } catch (error) {
+    console.log(directory);
+    console.log(filename);
+    if (fs.existsSync(`${directory}/${filename}`)) {
+      fs.unlinkSync(`${directory}/${filename}`);
+    }
     console.log(error.message); // Informações mais detalhadas apenas para os desenvolvedores
     response.status(400); // Erro dos dados informados pelo usuário
     return response.json({ message: "Erro ao criar post" }); // Msg padrão para o usuário
@@ -59,5 +80,5 @@ const CreatePost = async (request, response) => {
 };
 
 module.exports = {
-    CreatePost
-  };
+  CreatePost,
+};
